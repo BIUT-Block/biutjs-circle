adf929629fbce8990fc8b4f03ba14fba6ae5007c
theFay <fei.minhao@gmail.com>
2018-06-16 14:41:27 +0200

    test

--- test/secjs-circle-tester.js
+++ test/secjs-circle-tester.js
@@ -1,9 +1,23 @@
 const SECJSCircle = require('../src/secjs-circle')
 const should = require('chai').should()
+const assert = require('chai').assert
 
 describe('Circle', () => {
   describe('Intial Working Group Information', () => {
-    let circle = new SECJSCircle()
-    
+    it('Return the german ntp server', () => {
+      let circle = new SECJSCircle('DE')
+      assert.equal(circle.ntpTimeServerAddress, 'de.pool.ntp.org')
+    })
+
+    it('Return ntp server in other lands', () => {
+      let circle = new SECJSCircle('JP')
+      assert.equal(circle.ntpTimeServerAddress, 'de.pool.ntp.org')
+    })
+
+    it('Initial the time stamp of the last working group', () => {
+      let circle = new SECJSCircle('DE')
+      circle.initialCircle()
+      assert.equal(circle.timeStampOfLastGroup, Math.round((new Date()).getTime() / 1000))
+    })
   })
 })
